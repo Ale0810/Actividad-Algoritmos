58b87fb34fc57fd8c4144e1e017db2f2412a80e8
Ale0810 <alejandroverteche@gmail.com>
2024-09-26 18:53:37 -0300

    Cambios en las cartas

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,29 +1,38 @@
-import * as React from 'react';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import CardActionArea from '@mui/material/CardActionArea';
+import { Card, CardActionArea, CardMedia, CardContent, Typography } from '@mui/material';
 
 const CardDiseñada = (props) => {
   return (
-    <Card sx={{ maxWidth: { xs: 300, sm: 600, md: 1000 }, width: "100%" }}>
+    <Card
+      sx={{
+        maxWidth: { xs: 300, sm: 600, md: 1000 },
+        width: "100%",
+        height:"auto",
+        borderRadius: '12px', // Bordes redondeados
+        boxShadow: 3, // Sombra suave
+        transition: 'transform 0.2s', // Animación suave al hacer hover
+        '&:hover': {
+          transform: 'scale(1.05)', // Efecto de hover
+        },
+      }}
+    >
       <CardActionArea>
         <CardMedia
           component="img"
           sx={{
-            height:"20vh",
-            width:"20vw",
-            objectFit: 'cover'
+            height: "200px", // Ajusta la altura según sea necesario
+            width: "100%", // Asegúrate de que la imagen ocupe todo el ancho
+            objectFit: 'cover',
+            borderTopLeftRadius: '12px', // Bordes redondeados en la parte superior
+            borderTopRightRadius: '12px', // Bordes redondeados en la parte superior
           }}
           image={props.imagen}
           alt={props.texto}
         />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+         <CardContent sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+          <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold' }}>
             {props.titulo}
           </Typography>
-          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          <Typography variant="body2" sx={{ color: 'text.secondary', lineHeight: 1.5, flexGrow: 1 }}>
             {props.detalle}
           </Typography>
         </CardContent>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,7 +4,7 @@ import cerdito from "../Assets/Img/cerdito.jpg"
 const Home = () => {
   return (
     <>
-      <div className="flex flex-col justify-center items-center h-[85vh] ">
+      <div className="flex flex-col justify-center items-center h-[85vh]">
         <h1 className="text-5xl font-bold m-5">Bienvenido</h1>
         <div className="flex flex-col justify-center items-center shadow-2xl rounded-lg">
           <CardDiseñada
