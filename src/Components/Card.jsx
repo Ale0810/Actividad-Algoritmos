5cdce43d94f50435db90bf2502e4abf4d7e44e5f
Ale0810 <alejandroverteche@gmail.com>
2024-09-30 09:19:25 -0300

    Retoques en la carta

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,14 +1,23 @@
 import { Card, CardActionArea, CardMedia, CardContent, Typography } from '@mui/material';
+import { useEffect, useState } from 'react';
+import ModeEditRoundedIcon from '@mui/icons-material/ModeEditRounded';
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
 
 const CardDiseñada = (props) => {
+  const [usuario, setUsuario] = useState("");
+
+  useEffect(() => {
+
+  }, [usuario])
+
   return (
     <Card
       sx={{
-        maxWidth: { xs: 300, sm: 600, md: 1000 },
+        maxWidth: { xs: 150, sm: 170, md: 200 },
         width: "100%",
-        height:"auto",
+        height: "auto",
         borderRadius: '12px', // Bordes redondeados
-        boxShadow: 3, // Sombra suave
+        boxShadow: 4, // Sombra suave
         transition: 'transform 0.2s', // Animación suave al hacer hover
         '&:hover': {
           transform: 'scale(1.05)', // Efecto de hover
@@ -20,7 +29,7 @@ const CardDiseñada = (props) => {
           component="img"
           sx={{
             height: "200px", // Ajusta la altura según sea necesario
-            width: "100%", // Asegúrate de que la imagen ocupe todo el ancho
+            width: "100%", //
             objectFit: 'cover',
             borderTopLeftRadius: '12px', // Bordes redondeados en la parte superior
             borderTopRightRadius: '12px', // Bordes redondeados en la parte superior
@@ -28,13 +37,21 @@ const CardDiseñada = (props) => {
           image={props.imagen}
           alt={props.texto}
         />
-         <CardContent sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
           <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold' }}>
             {props.titulo}
           </Typography>
           <Typography variant="body2" sx={{ color: 'text.secondary', lineHeight: 1.5, flexGrow: 1 }}>
             {props.detalle}
           </Typography>
+          <div className="flex flex-row mt-2 justify-center items-center">
+            <div className='mr-2'>
+              <button className="rounded-lg bg-yellow-400 hover:bg-yellow-500 p-2">{<ModeEditRoundedIcon />}</button>
+            </div>
+            <div>
+              <button className="rounded-lg bg-red-400 hover:bg-red-500 p-2">{<DeleteForeverRoundedIcon />}</button>
+            </div>
+          </div>
         </CardContent>
       </CardActionArea>
     </Card>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,14 +6,12 @@ const Home = () => {
     <>
       <div className="flex flex-col justify-center items-center h-[85vh]">
         <h1 className="text-5xl font-bold m-5">Bienvenido</h1>
-        <div className="flex flex-col justify-center items-center shadow-2xl rounded-lg">
-          <CardDiseñada
-              imagen={cerdito}
-              texto="Cerdito Pepon"
-              titulo="Pepo"
-              detalle="La pepo"
-          />
-        </div>
+        <CardDiseñada
+          imagen={cerdito}
+          texto="Cerdito Pepon"
+          titulo="Pepo"
+          detalle="La pepo"
+        />
       </div>
     </>
   );
